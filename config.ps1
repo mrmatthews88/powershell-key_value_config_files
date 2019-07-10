046fb2f1036503e5052d127c860dd7b5c93c3e8c
mrmatthews88 <self@mrmatthews.me.uk>
2019-07-10 22:02:47 +0100

    code

---
+++ config.ps1
@@ -0,0 +1,38 @@
+﻿Function Get-Config([string]$filePath) {
+
+    $config = New-Module -AsCustomObject -ScriptBlock {
+        [string]$path = ".\Desktop\nnnn.txt"
+        [hashTable]$settings=@{}
+        Function load () {
+            (get-content $path) | % { 
+                $key, $value = $_.split("=")
+                if ($value) { 
+                    $settings[$key] = $value 
+                }
+            }
+        }
+
+        Function get([string]$key) {
+            return $settings[$key]
+        }
+
+        Function set([string]$key, $value) {
+            $settings[$key] = $value
+            save
+        }
+
+        Function save() {
+            "" > $path
+            $settings.keys | % {
+                $_ + "=" + $settings[$_] >> $path
+            }
+        }
+
+        Export-ModuleMember -Variable * -Function *
+    }
+    $config.path = $filePath
+    $config.load()
+    return $config
+}
+
+$myconfig = Get-Config -filePath ".\Desktop\config.txt"
